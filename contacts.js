85d011e5e7571bde755c9ebbed152664c3a3c1bc
Danil Deineka <tripledpadik@gmail.com>
2023-07-08 00:48:26 +0300

    Make reading all file and removing one contact

--- contacts.js
+++ contacts.js
@@ -2,3 +2,22 @@ import fs from "fs/promises";
 import path from "path";
 
 const contactsPath = path.resolve("db", "contacts.json");
+
+export async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.find((contact) => contact.id === contactId);
+  return contact || null;
+}
+
+export function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+export function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("Hi");
+import {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+// const argv = require("yargs").argv;
+
+// listContacts().then(console.log);
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const getById = await getContactById(id);
+      return console.log(getById);
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction({ id: "e6ywwRe4jcqxXfCZOj_1e", action: "get" });
